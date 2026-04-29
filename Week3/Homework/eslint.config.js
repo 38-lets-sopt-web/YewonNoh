1a8e4e31f9b8bf7f69785cbbafb4dc0fd1eac948
nohyewon <yw4913@naver.com>
2026-04-29 15:40:59 +0900

    chore: eslint, prettier 설정

--- Week3/Homework/eslint.config.js
+++ Week3/Homework/eslint.config.js
@@ -1,21 +1,46 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import react from 'eslint-plugin-react';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
+
   {
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
+      react.configs.flat.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettier,
     ],
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
+    plugins: {
+      react,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      eqeqeq: 'warn',
+      'dot-notation': 'warn',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'react/react-in-jsx-scope': 'off',
+      'react/destructuring-assignment': 'warn',
+      'react/jsx-pascal-case': 'warn',
+      'react/no-direct-mutation-state': 'warn',
+      'react/jsx-no-useless-fragment': 'warn',
+      'react/no-unused-state': 'warn',
+      'react/jsx-key': 'warn',
+      'react/self-closing-comp': 'warn',
+      'react/jsx-curly-brace-presence': 'warn',
+      'prettier/prettier': 'warn',
+    },
   },
-])
+]);
