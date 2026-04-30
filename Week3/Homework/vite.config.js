92e45e52deb77c21d88a36a11407ba6475968461
nohyewon <yw4913@naver.com>
2026-05-01 00:44:14 +0900

    chore: path alias 설정 추가

--- Week3/Homework/vite.config.js
+++ Week3/Homework/vite.config.js
@@ -9,6 +9,12 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
     },
   },
 });
