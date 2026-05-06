57a37a65c9324214752eb4a6d88dda307f822587
nohyewon <yw4913@naver.com>
2026-05-07 01:34:08 +0900

    :wrench:[chore]: vanilla-extract 설치 - tailwind css 제거

--- Week4/Homework/vite.config.ts
+++ Week4/Homework/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), vanillaExtractPlugin()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
